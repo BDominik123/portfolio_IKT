f7c7b58828829bbf94c7a8cee43f635a37d3418f
baloghdominik <baloghdominik8910@gmail.com>
2026-01-30 11:01:20 +0100

    futanari

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,11 +25,24 @@ const Linkaction = ()=>{
 navLink.forEach(n=> n.addEventListener('click', Linkaction))
 
 /*=============== HOME TYPED JS ===============*/
-
+var typed = new Typed('#home-subtitle', {
+    strings: ['Developer', 'Webdesigner', 'Frontend Manager'],
+    typeSpeed: 50,
+    loop:true,
+    backSpeed:40,
+    backDelay:2000,
+    cursorChar:'_'   
+    
+});
 
 /*=============== ADD SHADOW HEADER ===============*/
+const shadowHeader= ()=>{
+    const header = document.getElementById('header')
+    this.scrollY>50 ? header.classList.add('shadow-header'):
+    header.classList.remove('shadow_header')
+}
 
-
+window.addEventListener('scroll', shadowHeader)
 /*=============== CONTACT EMAIL JS ===============*/ 
 
 
