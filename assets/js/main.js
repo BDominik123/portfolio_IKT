6d8ab4dc01cc728af9faa28fed62c22e287b6fd9
baloghdominik <baloghdominik8910@gmail.com>
2026-01-30 08:54:14 +0100

    html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,29 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu')
-    navToggle = document.getElementById('nav-toggle')
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
     navClose = document.getElementById('nav-close')
 
 if(navToggle){
-    navToggle.addEventListener('click', ()=>{
+    navToggle.addEventListener('click',()=>{
         navMenu.classList.add('show-menu')
     })
 }
 
 if(navClose){
-    navClose.addEventListener('click', ()=>{
+    navClose.addEventListener('click',()=>{
         navMenu.classList.remove('show-menu')
     })
 }
+
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink= document.querySelectorAll('.nav__link')
 
 const Linkaction = ()=>{
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show-menu')
 }
-
 navLink.forEach(n=> n.addEventListener('click', Linkaction))
+
 /*=============== HOME TYPED JS ===============*/
 
 
